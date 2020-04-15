78dd63b40430a1fe321b5e025ecec73813b9a13f
lesves <lukas.veskrna@gmail.com>
2020-04-15 16:46:16 +0200

    Added singular, plural and death conditions

--- dracak/dracak.py
+++ dracak/dracak.py
@@ -1,6 +1,6 @@
 import pickle
 import random
-from story import stats, story, starts
+from story import stats, story, starts, death, names
 
 
 def satisfies(stats, requirements):
@@ -22,13 +22,29 @@ def missing(stats, requirements):
     return missing_items
 
 
+visited = set()
+
+name_singular = {}
+name_plural = {}
+for n in names:
+    name_singular[n] = names[n][0]
+    name_plural[n] = names[n][1]
+
+
 print("Vždy napiš číslo možnosti, jež zvolena tebou jest.")
 print("Pro zobrazení balancu tvého konta napiš money")
 
 place = random.choice(starts)
 while True:
-    if stats["health"] <= 0:
-        print("Přišel jsi o všechny životy...")
+    dead = False
+
+    for condition in death:
+        if stats[condition] <= 0:
+            print("Přišel jsi o všechny", name_plural[condition] + "...")
+            dead = True
+            break
+
+    if dead:
         print("Hra končí.")
         break
     
@@ -45,24 +61,32 @@ while True:
                 print(str(option+1) + ".", option_text)
                 print("nelze zvolit, chybí: ", end="")
                 for item, val in missing(stats, requirements).items():
-                    print(val, item, end=", ")
+                    if val == 1:
+                        print(val, name_singular[item], end=", ")
+                    else:
+                        print(val, name_plural[item], end=", ")
                 print()
 
         else:
             print(str(option+1) + ".", option_text)
 
+    visited.add(place)
+
     text = input()
     if text == "stats":
         for stat, value in stats.items():
-            print(stat + ":", value)
+            if value == 1:
+                print(name_singular[stat] + ":", value)
+            else:
+                print(name_plural[stat] + ":", value)
         continue
     elif text == "load":
         with open("save.pkl", "rb") as f:
             stats, place = pickle.load(f)
-    elif text in stats:
-        if not text.startswith("_"):
-            print("Máš", stats[text], text + ".")
-            continue
+    # elif text in stats:
+    #     if not text.startswith("_"):
+    #         print("Máš", stats[text], text + ".")
+    #         continue
     try:
         chose = int(text)-1
     except ValueError:
@@ -78,26 +102,37 @@ while True:
             print("Nesplňuješ požadavky.")
             continue
 
+    next_places = story[place][1][chose][1]
+    if isinstance(next_places, str):
+        next_place = next_places
+    else:
+        next_place = random.choice(next_places)
+
     for stat, change in story[place][1][chose][2].items():
+        if stat.startswith("first_"):
+            if next_place in visited:
+                continue
+            stat = stat[6:]
+
         if stat in stats:
             stats[stat] = stats[stat] + change
         else:
             stats[stat] = change
             
         if change == float("-inf"):
-            print("Přišel jsi o všechny", stat + ".")
+            print("Přišel jsi o všechny", name_plural[stat] + ".")
+        if change == -1:
+            print("Přišel jsi o", name_singular[stat] + ".")
         elif change < 0:
             print("Přišel jsi o", -change, stat + ".")
         elif change == 0:
             pass
+        elif change == 1:
+            print("Získal jsi", name_singular[stat] + ".")
         else:
             print("Získal jsi", change, stat + ".")
 
-    next_place = story[place][1][chose][1]
-    if isinstance(next_place, str):
-        place = next_place
-    else:
-        place = random.choice(next_place)
+    place = next_place
 
     with open("save.pkl", "wb") as f:
         pickle.dump([stats, place], f)
--- dracak/story.py
+++ dracak/story.py
@@ -1,16 +1,26 @@
 stats = {
     "money": 100,
     "health": 100,
+    "kidney": 2,
     }
+
+names = {
+    "money": ["zlaťák", "zlaťáky"],
+    "health": ["zdraví", "zdraví"],
+    "kidney": ["ledvina", "ledviny"],
+    "dagger": ["dýka", "dýky"],
+}
+
+death = ["health", "kidney"]
 
 story = {
     "ulice": ["Jsi na ulici", 
-              [["Půjdu na černý trh", "obchod", {}]]],
+              [["Půjdu na černý trh", "obchod", {"first_dagger": 1}]]],
     "obchod": ["Jsi na černém trhu.", 
-               [["Koupím si dýku za 10.", "obchod", {"dyka": 1, "money": -10}, {"money": 10}],
-                ["Prodám dýku za 7.", "obchod", {"dyka": -1, "money": 7}, {"dyka": 1}],
-                ["Koupím ledvinu za 20000.", "obchod", {"health": 50, "money": -20000}, {"money": 20000}],
-                ["Prodám ledvinu za 5000.", "obchod", {"health": -50, "money": 5000}],
+               [["Koupím si dýku za 10.", "obchod", {"dagger": 1, "money": -10}, {"money": 10}],
+                ["Prodám dýku za 7.", "obchod", {"dagger": -1, "money": 7}, {"dagger": 1}],
+                ["Koupím ledvinu za 20000.", "obchod", {"kidney": 1, "money": -20000}, {"money": 20000}],
+                ["Prodám ledvinu za 5000.", "obchod", {"kidney": -1, "money": 5000}],
                 ["Odejdu", "ulice", {}],]]
     }
 
